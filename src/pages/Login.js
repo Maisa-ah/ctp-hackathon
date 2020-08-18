79559db1423782d8f79b43b25bcd5bb64ff4124d
Maisa Ahmad <2013maisaah@gmail.com>
2020-08-18 11:56:54 -0400

    added fields component

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Field from '../components/Fields';
 
-function HomePage(props) {
+function LoginPage(props) {
   return (
     <div>
       <Field title="Email" name="Your email"/>
+      <Field title="First Name" name="First Name"/>
+      <Field title="Last Name" name="Last Name"/>
     </div>
   );
 }
 
-export default HomePage;
+export default LoginPage;
