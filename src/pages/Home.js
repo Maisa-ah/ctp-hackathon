4ec8dd6342845d8f99887d31c9470823d183248d
Maisa Ahmad <2013maisaah@gmail.com>
2020-08-18 11:18:02 -0400

    fixed layout of project and added field component

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Field from '../components/Fields'
+
+function HomePage(props) {
+  return (
+    <div>
+      <Field title="Email" name="Your email"/>
+      {/* Hello */}
+    </div>
+  );
+}
+
+export default HomePage;
