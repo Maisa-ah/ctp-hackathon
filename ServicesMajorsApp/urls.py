208575818c0e65dabf406f4039aa5437bdf180ce
PZDuck <agp2281337@gmail.com>
2020-08-18 20:19:29 -0400

    tweaked api views, corrected urls

--- ServicesMajorsApp/urls.py
+++ ServicesMajorsApp/urls.py
@@ -1,13 +1,16 @@
 from django.conf.urls import url
 from django.urls import path
-
 from . import views
 
+
 urlpatterns = [
-    path('', views.ListServices.as_view(), name='service_list'),
-    path('', views.ListServicesByType.as_view(), name='service_by_type_list'),
-    path('', views.ListMajors.as_view(), name='majors_list'),
-    path('', views.ListClasses.as_view(), name='classes_list'),
-    path('', views.ListClassesBySchool.as_view(), name='classes_by_schoo_list'),
-    path('', views.ListSchools.as_view(), name='schools_list'),
+    path('services/', views.ListServices.as_view(), name='list_service'),
+    path('services/<str:service_type>/', views.DetailService.as_view(), name='detail_service'),
+    path('majors/', views.ListMajors.as_view(), name='list_majors'),
+    path('majors/<str:major_code>/', views.DetailMajor.as_view(), name='detail_major'),
+    path('classes/', views.ListClasses.as_view(), name='list_classes'),
+    path('classes/<str:class_code>/', views.DetailClass.as_view(), name='detail_class'),
+    # path('classes/<str:school>/', views.ListClassesBySchool.as_view(), name='classes_by_school_list'),
+    path('schools/', views.ListSchools.as_view(), name='list_schools'),
+    path('schools/<str:school_code>/', views.DetailSchool.as_view(), name='detail_school'),
 ]
